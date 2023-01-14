ba492f04b311b1b52d71fe0ed153a68093c7d5a8
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 18:44:06 +0530

    Docs API Server

--- src/main/java/com/jat/HotelService/HotelServiceApplication.java
+++ src/main/java/com/jat/HotelService/HotelServiceApplication.java
@@ -1,13 +1,19 @@
 package com.jat.HotelService;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@ApiOperation("These Are Hotels API in OpenApi3 Format")
+//@ApiOperation("These Are Hotels API in OpenApi3 Format")
+@OpenAPIDefinition(info = @Info(title = "Hotel API", version = "3.0", description = "It contains Hotel Information along with ratings & other criteria. API is powered by salient features of Spring Boot e.g. resilence4j, ratelimiters, circuitbreakers" +
+		"and further to specify by Feign client. The api will be being consumed by Rating Services Api & finally Hotel Services API. It is already registered on Eurka server " +
+		"https://serviceregitsry-production.up.railway.app/ whereas planned to be protected by Okta Auth & JWT"))
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
