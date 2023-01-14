eb0d79bfe24c528fdf7378165c8a3315facea5a1
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 19:39:41 +0530

    Docs API Server

--- src/main/java/com/jat/HotelService/HotelServiceApplication.java
+++ src/main/java/com/jat/HotelService/HotelServiceApplication.java
@@ -10,9 +10,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@OpenAPIDefinition(info = @Info(title = "This Hotel API Microservices. Please Choose another spec from dropdown for other services", version = "3.0", description = "It contains Hotel Information along with ratings & other criteria. API is powered by salient features of Spring Boot e.g. resilence4j, ratelimiters, circuitbreakers" +
-		"and further to specify by Feign client. The api will be being consumed by Rating Services Api & finally Hotel Services API. It is already registered on [Eurka server]" +
-		"(!https://serviceregitsry-production.up.railway.app/) whereas planned to be protected by Okta Auth & JWT"))
+@OpenAPIDefinition(info = @Info(title = "***  Hotel Microservices   ***. \n Please Choose another spec from dropdown for other services from main Document Server API Gatway", version = "3.0", description = "It contains Hotel Information along with ratings & other criteria. API is powered by salient features of Spring Boot e.g. [resilence4j](!https://resilience4j.readme.io/docs),"+ 
+"[ratelimiters](!https://resilience4j.readme.io/docs/ratelimiter), [circuitbreakers](!https://resilience4j.readme.io/docs/circuitbreaker)" +
+		"and further to specify by Feign client. The api will be being consumed by Rating Services Api & finally Hotel Services API. It is already registered on [Eurka server], " +
+		"(!https://serviceregitsry-production.up.railway.app/), Spring Cloud API-Gateway, Configuration Server & a Combined Document Server. We are also Implementing protection by [Okta Auth](!https://developer.okta.com/docs/reference/api/authn/), [Spring Security](!https://spring.io/projects/spring-security)  & [JSON Web Based Tokens](!https://www.baeldung.com/spring-security-oauth-jwt)"))
 @ApiOperation("It is prepared with java 17, spring version 2.7.7 enabled with EUREKA SERVER, CONFIG-SERVER,API-GATWAY, DOCUMENT SERVER & SPING SECURITY with OKTA AUTH")
 public class HotelServiceApplication {
 
