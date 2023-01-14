989b0cc57784522cc9613405c082e6e4d2d10dea
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 19:20:01 +0530

    Docs API Server

--- src/main/java/com/jat/HotelService/HotelServiceApplication.java
+++ src/main/java/com/jat/HotelService/HotelServiceApplication.java
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@OpenAPIDefinition(info = @Info(title = "This Hotel API Microservices", version = "3.0", description = "It contains Hotel Information along with ratings & other criteria. API is powered by salient features of Spring Boot e.g. resilence4j, ratelimiters, circuitbreakers" +
-		"and further to specify by Feign client. The api will be being consumed by Rating Services Api & finally Hotel Services API. It is already registered on Eurka server " +
-		"https://serviceregitsry-production.up.railway.app/ whereas planned to be protected by Okta Auth & JWT"))
+@OpenAPIDefinition(info = @Info(title = "This Hotel API Microservices. Please Choose another spec from dropdown for other services", version = "3.0", description = "It contains Hotel Information along with ratings & other criteria. API is powered by salient features of Spring Boot e.g. resilence4j, ratelimiters, circuitbreakers" +
+		"and further to specify by Feign client. The api will be being consumed by Rating Services Api & finally Hotel Services API. It is already registered on [Eurka server]" +
+		"(!https://serviceregitsry-production.up.railway.app/) whereas planned to be protected by Okta Auth & JWT"))
 @ApiOperation("It is prepared with java 17, spring version 2.7.7 enabled with EUREKA SERVER, CONFIG-SERVER,API-GATWAY, DOCUMENT SERVER & SPING SECURITY with OKTA AUTH")
 public class HotelServiceApplication {
 
