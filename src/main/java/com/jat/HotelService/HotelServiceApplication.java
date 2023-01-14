8e8923a59bf1afe13bec87443e1beb4ac527edf5
- <p28@LAMU0Q7GF7X05W6.uhc.com>
2023-01-14 14:18:24 +0530

    Docs API Server

--- src/main/java/com/jat/HotelService/HotelServiceApplication.java
+++ src/main/java/com/jat/HotelService/HotelServiceApplication.java
@@ -1,11 +1,13 @@
 package com.jat.HotelService;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ApiOperation("These Are Hotels API in OpenApi3 Format")
 public class HotelServiceApplication {
 
 	public static void main(String[] args) {
